a1a0e4605d6042e6dbf4e5893a9e2d6ea8c0c9a1
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-11-02 17:26:49 +0100

    remove registry stuff

--- lint-helm-chart/entrypoint.sh
+++ lint-helm-chart/entrypoint.sh
@@ -8,9 +8,6 @@ export HELM_EXPERIMENTAL_OCI=1
 # switch to working directory
 cd ${INPUT_WORKING_DIRECTORY}
 
-# log into helm registry
-helm registry login -u "${INPUT_HELM_REGISTRY_USER}" -p "${INPUT_HELM_REGISTRY_TOKEN}" ${INPUT_HELM_REGISTRY_URL}
-
 # lint helm chart
 echo "::group::Lint helm chart"
 helm lint .
