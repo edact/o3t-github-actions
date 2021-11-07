ac915e47f2846f44754ba1e9c46080288baae461
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-11-07 16:44:11 +0100

    no check

--- run-newman-cli/entrypoint.sh
+++ run-newman-cli/entrypoint.sh
@@ -3,8 +3,6 @@
 # makes the script existing once an error occours
 set -euo pipefail
 
-curl --fail https://api.getpostman.com/collections/${INPUT_POSTMAN_COLLECTION_ID}?apikey=${INPUT_POSTMAN_TOKEN} | (echo "::error::Collection could not be fetched."; exit 1)
-
 # install newman & html reporter
 npm install newman newman-reporter-htmlextra -g
 
