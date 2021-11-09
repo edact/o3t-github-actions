39a1a65319f8f463c9ae7db3d1c6dbaf5762da8f
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-11-09 23:07:18 +0100

    work

--- create-npmrc-file/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# makes the script existing once an error occours
-set -euo pipefail
-
-printf "${INPUT_NPM_SCOPE}:registry=https://${INPUT_NPM_REGISTRY_URL}/ \n //${INPUT_NPM_REGISTRY_URL}/:_authToken=${INPUT_NPM_TOKEN}"> .npmrc
--- lint-helm-chart/entrypoint.sh
+++ lint-helm-chart/entrypoint.sh
@@ -8,10 +8,6 @@ export HELM_EXPERIMENTAL_OCI=1
 # switch to working directory
 cd ${INPUT_WORKING_DIRECTORY}
 
-RUN apk add curl bash openssl
-RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
-RUN chmod 700 get_helm.sh && ./get_helm.sh
-
 # lint helm chart
 echo "::group::Lint helm chart"
 helm lint .
